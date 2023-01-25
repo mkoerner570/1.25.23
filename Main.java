e0f21259473e7a4edd52850476589b182cfeea12
Max <mkoerner570@gmail.com>
2023-01-25 18:38:24 -0500

    if conditional for leap year

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
+//package edu.cscc
+
 import java.util.*;
+import java.util.Scanner;
 
 class Main {
+
+    private static Scanner input = new Scanner(System.in);
     public static void main (String[] args){
-        System.out.println("hello world!");
+        // System.out.println("hello world!");
+        int year;
+        String result;
+
+        System.out.println("Leap year calculator \n please enter a year: ");
+        year = input.nextInt();
+        // System.out.println(year % 4);
+
+        if (year % 4 == 0){
+            System.out.println("It is a leap year!");
+        }
+
     }
 }
