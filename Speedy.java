0a7e66a902da4dd3288c43c103f9e97ab354e37d
Max <mkoerner570@gmail.com>
2023-01-25 19:00:03 -0500

    speedy file created

---
+++ Speedy.java
@@ -0,0 +1,10 @@
+//package edu.cscc
+
+import java.util.*;
+import java.util.Scanner;
+
+class Speedy {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
